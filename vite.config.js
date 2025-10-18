507ab1eccd6ea210fad1e01b63f34e43a3043979
SANHITHA DUDDUKURU <sanhithachowdary06@gmail.com>
2025-10-18 09:59:10 +0530

    Fix: set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: '/s104-insem1-2300030173_Dudukukru_Sanhitha/',
 })
