ee0d708f9b1bd5c6a32ff198300ca7f97263a7f5
SANHITHA DUDDUKURU <sanhithachowdary06@gmail.com>
2025-10-18 10:09:21 +0530

    Fix GitHub Pages deployment for Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
