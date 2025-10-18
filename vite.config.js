c2700bea4877079074db072836cefe769cf95306
SANHITHA DUDDUKURU <sanhithachowdary06@gmail.com>
2025-10-18 10:23:48 +0530

    Fix GitHub Pages deployment for Vite

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
       base: '/s104-insem1-2300030173_Duddukuru_Sanhitha/', // 👈 your repo name
+    
 })
